466beebc765fb0138070a39e7cee910eb5711dfd
wtayyeb <wtayyeb@users.noreply.github.com>
2017-08-30 11:44:10 +0430

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,5 +3,5 @@
 cd /home/user1/pr1-venv/pr1
 git pull origin master
 /home/user1/pr1-venv/bin/pip install -U -r /home/user1/pr1-venv/pr1/requirement.txt
-supervisorctl stop pr1-wsgi
-supervisorctl start pr1-wsgi
+supervisorctl stop pr1_uwsgi
+supervisorctl start pr1_uwsgi
